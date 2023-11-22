874a96e1b5d7f4954e1fd50a05f021a0caf81bf3
hakurennnn <ranpinas@proton.me>
2023-11-22 22:34:37 +0800

    chat like

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { StatusBar, Dimensions, Switch } from 'react-native';
+import { StatusBar, Dimensions, Switch, TextInput, FlatList } from 'react-native';
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import { FontAwesome5 } from '@expo/vector-icons';
+import Voice from 'react-native-voice';
 
 const { width, height } = Dimensions.get('window');
 
 export default function App() {
   const [loading, setLoading] = useState(true);
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [chat, setChat] = useState([]);
+  const [text, setText] = useState('');
 
   useEffect(() => {
     const timer = setTimeout(() => setLoading(false), 2000);
@@ -16,7 +19,51 @@ export default function App() {
 
   const toggleDarkMode = () => setIsDarkMode((prevMode) => !prevMode);
 
-  const buttonSize = Math.min(width, height) * 0.2;
+  const buttonWidth = Math.min(width, height) * 0.2;
+  const buttonHeight = 40;
+  const buttonBorderRadius = 10;
+
+  const handleChangeText = (inputText) => {
+    setText(inputText);
+  };
+
+  const handleSubmit = () => {
+    if (text.trim() !== '') {
+      setChat([...chat, { id: chat.length, text, isUser: true }]);
+      setText('');
+      // Add any logic here to handle responses or perform actions based on the submitted text
+    }
+  };
+
+  // Voice
+  const [isRecording, setIsRecording] = useState(false);
+  const [recognizedText, setRecognizedText] = useState('');
+
+  const startRecording = async () => {
+    try {
+      await Voice.start('en-US'); // Start recording with specified language
+      Voice.onSpeechStart = () => {
+        setIsRecording(true);
+      };
+      Voice.onSpeechResults = (e) => {
+        setRecognizedText(e.value[0]); // Set recognized text
+      };
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    Voice.onSpeechEnd = () => {
+      setIsRecording(false);
+      if (recognizedText.trim() !== '') {
+        setChat([...chat, { id: chat.length, text: recognizedText, isUser: false }]);
+      }
+    };
+    return () => {
+      Voice.destroy().then(Voice.removeAllListeners);
+    };
+  }, [recognizedText, chat]);
 
   const styles = StyleSheet.create({
     container: {
@@ -54,27 +101,71 @@ export default function App() {
     buttonContainer: {
       flexDirection: 'row',
       justifyContent: 'space-around',
-      marginTop: 20,
-      height: buttonSize + 40,
+      alignItems: 'center',
+      marginTop: 10, // Adjusted gap
     },
     button: {
-      width: buttonSize,
-      height: buttonSize,
-      padding: 20,
-      borderRadius: 5,
-      borderWidth: 7,
+      flex: 1,
+      height: buttonHeight,
+      borderRadius: buttonBorderRadius,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderWidth: 2,
       borderColor: '#9999FF',
       backgroundColor: '#9999FF',
     },
-    buttonLabel: {
-      position: 'absolute',
-      bottom: 0,
-      left: 0,
-      right: 0,
-      textAlign: 'left',
+    microphoneIcon: {
+      fontSize: buttonHeight * 0.5,
       color: 'white',
-      fontSize: 10,
-      fontWeight: 'medium',
+    },
+    buttonLabel: {
+      color: 'white',
+      fontSize: 12,
+    },
+    inputContainer: {
+      flexDirection: 'row',
+      alignItems: 'baseline',
+      marginTop: 10, // Adjusted gap
+    },
+    input: {
+      flex: 1,
+      width: '80%',
+      borderColor: 'gray',
+      borderWidth: 1,
+      borderRadius: 5,
+      padding: 10,
+      marginBottom: 10,
+    },
+    submitbutton: {
+      width: '20%', 
+      height: buttonHeight,
+      backgroundColor: '#9999FF',
+      borderRadius: 5,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginLeft: 10,
+      padding: 10,
+    },
+    submitbuttonText: {
+      color: 'white',
+      fontSize: 12,
+    },
+    chatBubble: {
+      padding: 10,
+      margin: 5,
+      borderRadius: 10,
+      maxWidth: '80%',
+    },
+    userBubble: {
+      backgroundColor: '#9999FF',
+      alignSelf: 'flex-end',
+    },
+    botBubble: {
+      backgroundColor: '#6666FF',
+      alignSelf: 'flex-start',
+    },
+    bubbleText: {
+      color: 'white',
     },
   });
 
@@ -95,29 +186,40 @@ export default function App() {
       </View>
       {loading ? (
         <Text>
-          <Text style={styles.sirenText}>Siren</Text>
-          <Text style={styles.senseText}>SENSE</Text>
+          <Text style={styles.sirenText}>Speech2</Text>
+          <Text style={styles.senseText}>TEXT</Text>
         </Text>
       ) : (
         <View>
+          <FlatList
+            data={chat}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => (
+              <View
+                style={[
+                  styles.chatBubble,
+                  item.isUser ? styles.userBubble : styles.botBubble,
+                ]}
+              >
+                <Text style={styles.bubbleText}>{item.text}</Text>
+              </View>
+            )}
+          />
           <View style={styles.buttonContainer}>
-            <TouchableOpacity style={[styles.button, { marginRight: 10 }]}>
-              <FontAwesome5 name="satellite-dish" size={buttonSize * 0.6} color="white" />
-              <Text style={[styles.buttonLabel, { fontSize: buttonSize * 0.1 }]}>Satellite Dish</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button}>
-              <FontAwesome5 name="map" size={buttonSize * 0.6} color="white" />
-              <Text style={[styles.buttonLabel, { fontSize: buttonSize * 0.1 }]}>Map</Text>
+            <TouchableOpacity style={[styles.button]} onPress={startRecording}>
+              <FontAwesome5 name="microphone" style={styles.microphoneIcon} />
+              <Text style={styles.buttonLabel}>{isRecording ? 'Stop Recording' : 'Microphone'}</Text>
             </TouchableOpacity>
           </View>
-          <View style={styles.buttonContainer}>
-            <TouchableOpacity style={[styles.button, { marginRight: 10 }]}>
-              <FontAwesome5 name="clipboard" size={buttonSize * 0.6} color="white" />
-              <Text style={[styles.buttonLabel, { fontSize: buttonSize * 0.1 }]}>Clipboard</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={[styles.button, { marginRight: 10 }]}>
-              <FontAwesome5 name="address-book" size={buttonSize * 0.6} color="white" />
-              <Text style={[styles.buttonLabel, { fontSize: buttonSize * 0.1 }]}>Contacts</Text>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              onChangeText={handleChangeText}
+              value={text}
+              placeholder="Enter text here..."
+            />
+            <TouchableOpacity style={styles.submitbutton} onPress={handleSubmit}>
+              <Text style={styles.submitbuttonText}>Submit</Text>
             </TouchableOpacity>
           </View>
         </View>
